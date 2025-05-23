a7723bcd5918623782f96bf6bb1dccc7e409a63b
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-05-22 23:40:40 -0300

    Adds (UI) Text component

---
+++ src/ui/text/index.tsx
@@ -0,0 +1,21 @@
+//! Types
+import { TextProps } from '../types';
+
+//! Styles
+import styles from "./styles.module.css"
+
+export default function Text({ variant, children }: TextProps) {
+  const isTitle = variant === 'title';
+  const isSubtitle = variant === 'subtitle';
+  const isParagraph = variant === 'paragraph';
+  const isDetails = variant === 'details';
+
+  return (
+    <>
+      {isTitle && <h2 className={styles.title}>{children}</h2>}
+      {isSubtitle && <h3 className={styles.subtitle}>{children}</h3>}
+      {isParagraph && <p className={styles.paragraph}>{children}</p>}
+      {isDetails && <p className={styles.details}>{children}</p>}
+    </>
+  );
+}
