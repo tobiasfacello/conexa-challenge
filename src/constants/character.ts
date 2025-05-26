d93c67cb2f7589f58e74642d22089208beeb4186
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-05-26 19:55:58 -0300

    Adds global constants declarations

---
+++ src/constants/character.ts
@@ -0,0 +1,21 @@
+//? Character Related Constants
+
+export const STATUS_EMOJIS: Record<string, string> = {
+	Alive: '🟢',
+	Dead: '🔴',
+	default: '⚫',
+};
+
+export const SPECIES_EMOJIS: Record<string, string> = {
+	Human: '👨‍💼',
+	Alien: '👽',
+	Humanoid: '🧝',
+	Robot: '🤖',
+	Animal: '🐾',
+	'Mythological Creature': '🦄',
+	Poopybutthole: '💩',
+	Cronenberg: '🧟',
+	Disease: '🦠',
+	Planet: '🪐',
+	default: '❓',
+};
