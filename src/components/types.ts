42d5ce2ae38b7bca0ea839603ce43c902ffadd58
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-05-26 20:07:22 -0300

    Adds Components' types declarations

---
+++ src/components/types.ts
@@ -0,0 +1,45 @@
+//! Types
+import {
+	CharacterSchema,
+	EpisodeSchema,
+	SelectedCharacterType,
+	EpisodesUrlType,
+} from '@/types';
+
+//* Characters List
+export type CharactersListProps = {
+	title: string;
+	description: string;
+	selectedCharacter: SelectedCharacterType | null;
+	setCharacter: (character: SelectedCharacterType) => void;
+	setEpisodes: (episodes: EpisodesUrlType) => void;
+};
+
+//* Character Card
+export interface CharacterCardProps
+	extends Pick<
+		CharacterSchema,
+		'id' | 'image' | 'name' | 'status' | 'species' | 'episode'
+	> {
+	selectedCharacter: SelectedCharacterType | null;
+	setCharacter: (character: SelectedCharacterType) => void;
+	setEpisodes: (episodes: EpisodesUrlType) => void;
+}
+
+//* Episodes List
+export type EpisodesListProps = {
+	title: string;
+	episodesList: EpisodesUrlType;
+};
+
+//* Episode Card
+export type EpisodeCardProps = Pick<
+	EpisodeSchema,
+	'id' | 'name' | 'air_date' | 'characters'
+>;
+
+//* Pagination
+export type PaginationProps = {
+	pageCount: number;
+	onPageChange: (data: { selected: number }) => void;
+};
